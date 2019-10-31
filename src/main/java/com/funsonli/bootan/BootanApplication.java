ae2ec1f6814f1797cbbb4216444c5922b835e4c1
funsonli <funsonli@163.com>
2019-10-31 09:25:33 +0800

    init

---
+++ src/main/java/com/funsonli/bootan/BootanApplication.java
@@ -0,0 +1,13 @@
+package com.funsonli.bootan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootanApplication.class, args);
+    }
+
+}
